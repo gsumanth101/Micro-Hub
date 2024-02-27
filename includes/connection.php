8a82746b9248d04d24df7cb9d9069566bd400523
G Sumanth <surekhabrundavanamcm@gmail.com>
2024-02-28 05:25:58 +0530

    i

--- includes/connection.php
+++ includes/connection.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "us-cluster-east-01.k8s.cleardb.net";
-$username = "bdbd8cf3909997"; 
-$password = "ab26be31"; 
-$dbname = "heroku_a206025092379f3";
-
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:microhub.database.windows.net,1433; Database = sdt", "CloudSAd65eb16b", "Sumanth@12345");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "CloudSAd65eb16b", "pwd" => "{your_password_here}", "Database" => "sdt", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:microhub.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
